6b8cb26e22922e4765b1ea3f444e4b7c0a1d70bf
PaolaMendozaV <paolamendozavidal@gmail.com>
2021-06-08 16:56:37 -0400

    Feat: user components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import User from './components/User';
+
 
 function App() {
+  const name = "Paola";
   return (
-    <div className="App">
+    <div>
+      <User />
+
       
     </div>
   );
