3966a75047f482db0d5100d5e76959c4810ffbee
Camila Castro <ccastromn@gmail.com>
2021-06-09 09:26:24 -0400

    Chore: Bootstrap-react

--- src/components/productsList.js
+++ src/components/productsList.js
@@ -1,9 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function ProductList() {
     return (
       <div className="App">
         <h1>Lista de productos</h1>
         <div>
-            <button>Pan masa madre</button>
+            <button className = "btn btn-dark">Pan masa madre</button>
             <button>Pasteles</button>
         </div>
       </div>
