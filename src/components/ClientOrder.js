07418a41827d917f7d4aaa7ea22dfcf88eafc1a0
Camila Castro <ccastromn@gmail.com>
2021-06-14 10:45:40 -0400

    Feat: Creation add products to client order

--- src/components/ClientOrder.js
+++ src/components/ClientOrder.js
@@ -1,11 +1,56 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Table, Container, Row, Col } from 'react-bootstrap';
+import { useState } from "react";
+import ProductsSection from "./ProductsSection";
 
 function ClientOrder() {
-  return (
-    <div>
-        <h1>ORDEN DEL CLIENTE</h1>
-    </div>
-  )
+    //El estado de  valor inicial,
+    //DEfinir un estado
+    const [orderEntries, setOrderEntries] = useState([]); 
+    //Crear funcion para setear orderEntries
+    const addEntry = (product) =>{
+        setOrderEntries([...orderEntries, product]); // setear un nuevo estado con lo que habia antes + en nuevo producto
+    }
+    return (
+        <Container>
+        <Row>
+          <Col >
+          <ProductsSection addEntry={addEntry}/>
+          </Col>
+          <Col >
+          <div>
+            <h1>ORDEN DEL CLIENTE</h1>
+            <Table striped bordered hover size="sm">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Productos</th>
+                        <th>Precio</th>
+                        <th>Eliminar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        orderEntries.map((orderEntry)=>{
+                            return (
+                            <tr>
+                            <td>1</td>
+                            <td>{orderEntry.name}</td>
+                            <td>{orderEntry.price}</td>
+                            <td>Eliminar</td>
+                            </tr>
+                            );
+                        })
+                    }
+                </tbody>
+            </Table>
+        </div>
+          </Col>
+        </Row>
+
+      </Container>
+
+    )
 }
 
 export default ClientOrder;
